6577a7bfe6ba281e9569bc4acca1a59ae51e784e
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-08 21:03:31 +0530

    Added Menu component

---
+++ pizza-menu-app/src/components/Menu.tsx
@@ -0,0 +1,11 @@
+import Pizza from "./Pizza";
+
+export function Menu() {
+  return (
+    <div>
+      <h1>Menu</h1>
+      <p>Check out our delicious pizzas!</p>
+      <Pizza />
+    </div>
+  );
+}
--- pizza-menu-app/src/components/Pizza.tsx
+++ pizza-menu-app/src/components/Pizza.tsx
@@ -1,9 +1,9 @@
 export default function Pizza() {
   return (
     <div>
-        <h2>Pizza</h2>
+        <h2>Pizza : Pizza Spinaci</h2>
         <img src="/pizzas/spinaci.jpg" alt="Spinaci Pizza" />
-        <p>This is a simple pizza component.</p>
+        <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
 }
