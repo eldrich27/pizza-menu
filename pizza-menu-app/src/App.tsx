522968844f2268cdf6e854a10ec5b872ec64f66f
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-08 20:45:31 +0530

    Added a Pizza component

--- pizza-menu-app/src/App.tsx
+++ pizza-menu-app/src/App.tsx
@@ -1,8 +1,13 @@
+import Pizza from "./components/Pizza";
+
+
 export default function App() {
   return (
     <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
       <h1>Hello, World!</h1>
       <p>Clean setup running from scratch.</p>
+      <Pizza />
+      <Pizza />
     </main>
   );
 }
--- pizza-menu-app/src/components/Pizza.tsx
+++ pizza-menu-app/src/components/Pizza.tsx
@@ -1,8 +1,9 @@
 export default function Pizza() {
   return (
     <div>
-      <h2>Pizza</h2>
-      <p>This is a simple pizza component.</p>
+        <h2>Pizza</h2>
+        <img src="/pizzas/spinaci.jpg" alt="Spinaci Pizza" />
+        <p>This is a simple pizza component.</p>
     </div>
   );
 }
