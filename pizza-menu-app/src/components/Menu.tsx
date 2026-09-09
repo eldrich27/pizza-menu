120e56bbe83983cd84169422e054436cbb7a2225
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-09 10:26:01 +0530

    Adding props to pizza component

--- pizza-menu-app/src/components/Menu.tsx
+++ pizza-menu-app/src/components/Menu.tsx
@@ -1,13 +1,14 @@
 import Pizza from "./Pizza";
+import "../data.js";
 
 export function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
       <p>Check out our delicious pizzas!</p>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza name="Spinaci" ingredients="Tomato, mozarella, spinach, and ricotta cheese" price={12.99} />
+      <Pizza name="Margherita" ingredients="Tomato, mozarella, and basil" price={10.99} />
+      <Pizza name="Pepperoni" ingredients="Tomato, mozarella, and pepperoni" price={13.99} />
     </main>
   );
 }
--- pizza-menu-app/src/components/Pizza.tsx
+++ pizza-menu-app/src/components/Pizza.tsx
@@ -1,9 +1,16 @@
-export default function Pizza() {
+interface PizzaProps {
+    name: string;
+    ingredients: string;
+    price: number;
+}
+
+export default function Pizza({ name, ingredients, price }: PizzaProps) {
   return (
     <div>
         <img src="/pizzas/spinaci.jpg" alt="Spinaci Pizza" />
-        <h3>Pizza : Pizza Spinaci</h3>
-        <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+        <h3>Pizza : {name}</h3>
+        <p>{ingredients}</p>
+        <p>Price: ${price.toFixed(2)}</p>
     </div>
   );
 }
