42171d729ab5244f7976f69ea81bbefeb9988949
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-09 10:30:36 +0530

    Adding images to the props

--- pizza-menu-app/src/components/Menu.tsx
+++ pizza-menu-app/src/components/Menu.tsx
@@ -6,9 +6,9 @@ export function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
       <p>Check out our delicious pizzas!</p>
-      <Pizza name="Spinaci" ingredients="Tomato, mozarella, spinach, and ricotta cheese" price={12.99} />
-      <Pizza name="Margherita" ingredients="Tomato, mozarella, and basil" price={10.99} />
-      <Pizza name="Pepperoni" ingredients="Tomato, mozarella, and pepperoni" price={13.99} />
+      <Pizza image="/pizzas/spinaci.jpg" name="Spinaci" ingredients="Tomato, mozarella, spinach, and ricotta cheese" price={12.99} />
+      <Pizza image="/pizzas/margherita.jpg" name="Margherita" ingredients="Tomato, mozarella, and basil" price={10.99} />
+      <Pizza image="/pizzas/focaccia.jpg" name="Focaccia" ingredients="Tomato, mozarella, and herbs" price={11.99} />
     </main>
   );
 }
--- pizza-menu-app/src/components/Pizza.tsx
+++ pizza-menu-app/src/components/Pizza.tsx
@@ -2,12 +2,13 @@ interface PizzaProps {
     name: string;
     ingredients: string;
     price: number;
+    image: string; // Optional image prop
 }
 
-export default function Pizza({ name, ingredients, price }: PizzaProps) {
+export default function Pizza({image, name, ingredients, price }: PizzaProps) {
   return (
     <div>
-        <img src="/pizzas/spinaci.jpg" alt="Spinaci Pizza" />
+        <img src={image} alt={name} />
         <h3>Pizza : {name}</h3>
         <p>{ingredients}</p>
         <p>Price: ${price.toFixed(2)}</p>
