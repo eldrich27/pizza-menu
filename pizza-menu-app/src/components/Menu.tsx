696b794da79f95933e8199f03e010daddc575369
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-10 09:09:40 +0530

    Added logic for returning empty menu if menu data is nt available

--- pizza-menu-app/src/components/Menu.tsx
+++ pizza-menu-app/src/components/Menu.tsx
@@ -2,16 +2,23 @@ import Pizza from "./Pizza";
 import pizzaData from "../data";
 
 export function Menu() {
+//   const pizzas= pizzaData;
+  const pizzas:any = [];
+  const numPizzas: number = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
       <p>Check out our delicious pizzas!</p>
-
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza key={pizza.name} pizza={pizza} />
-        ))}
-      </ul>
+      {
+        numPizzas > 0 && (
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+            <Pizza key={pizza.name} pizza={pizza} />
+            ))}
+          </ul>
+        )
+      };
     </main>
   );
 }
