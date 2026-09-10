336a8d3a1b774ed4fa7c3e2888ad8266480dd39b
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-10 09:19:44 +0530

    Added React fragments to return multiple html elements tgether

--- pizza-menu-app/src/components/Menu.tsx
+++ pizza-menu-app/src/components/Menu.tsx
@@ -1,24 +1,29 @@
+import React from "react";
 import Pizza from "./Pizza";
 import pizzaData from "../data";
 
 export function Menu() {
-//   const pizzas= pizzaData;
-  const pizzas:any = [];
+  const pizzas= pizzaData;
+//   const pizzas:any = [];
   const numPizzas: number = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <p>Check out our delicious pizzas!</p>
+      
       {
-        numPizzas > 0 && (
-          <ul className="pizzas">
-            {pizzas.map((pizza) => (
-            <Pizza key={pizza.name} pizza={pizza} />
-            ))}
-          </ul>
-        )
-      };
+        numPizzas > 0 ? (
+          <React.Fragment>
+            <p>Check out our delicious pizzas!</p>
+            <ul className="pizzas">
+              {pizzas.map((pizza) => (
+              <Pizza key={pizza.name} pizza={pizza} />
+              ))}
+            </ul>
+          </React.Fragment>
+        ) : (
+          <span>We're still working on our menu. Please check back later!</span>
+        )};
     </main>
   );
 }
